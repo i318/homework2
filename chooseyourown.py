7de62b3309129bc38b44bc44dffa23dd971fd56b
i318 <78052716+i318@users.noreply.github.com>
2022-04-12 14:39:13 -0700

    Add files via upload

---
+++ chooseyourown.py
@@ -0,0 +1,27 @@
+
+
+
+from tkinter import N
+
+
+name = input("Enter your username: ")
+if name.isalpha() == False:
+    print("Use alphabetical letters only. ")
+
+
+
+print("Welcome,", name,". From here on, you shall embark on your journey to reveal your final fate! Based on the book All Tomorrow's. " )
+
+answer = input("Humanity is doomed. The Qu have survived every atomic bomb sent by the humans. You are in charge of making the final call.\
+    Do you want to unleash a highly developed plague (1) to weaken the Qu, or use mecha suits?(2) Choose 1 or 2:  ")  
+
+if answer == "1":
+    a2= input("It was highly ineffetive. The Qu do not have weak immune systems like humans. They bioengineered their DNA to be flawless. Now, you lost the hopes of people who believed you could save the world. You walk down the street after this failure and see a glowing powder on the ground.\
+        Do you want to pick it up (1) or not (2):  ")
+        if a2 == "1"
+        n 
+        
+elif answer == "2":
+    print("The mecha suits you engineered were strangely effective. The Qu population decreased by 10% thanks to your expertise. Now, do you want to celebrate(1) or not (2): ")
+else: 
+    print("You died of stupidity")
